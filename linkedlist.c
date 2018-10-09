c0fb6ac61a87b048c965058ae9b91c3fb0a63200
Brian <hlee5@stuy.edu>
2018-10-09 15:28:12 -0400

    free_list() and insert_front()

--- linkedlist.c
+++ linkedlist.c
@@ -1,34 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct node { int i; struct node *next;};
 
 void print_list(struct node *head)
 {
-  /*
-   * Prints all values in a linked list
-   * The pointer to the next node of the last node must be a null pointer,
-   * or else it will probably do unexpected things
-   */
-  struct node *cur_node = head;
-  while (cur_node)
-  {
-    printf("%d\n", cur_node->i);
-    cur_node = cur_node->next;
-  };
-  return;
+    /*
+     * Given a pointer to the node representing the first node in a linked list,
+     * prints all values in the linked list.
+     * The linked list must be terminated by a null pointer.
+     */
+    struct node *cur_node = head;
+    while (cur_node)
+    {
+        printf("%d\n", cur_node->i);
+        cur_node = cur_node->next;
+    };
+    return;
 }
 
-int main()
+struct node * insert_front(struct node *target, int i)
 {
-  struct node a;
-  struct node b;
-  struct node c;
-  a.i = 42;
-  a.next = &b;
-  b.i = 300;
-  b.next = &c;
-  c.i = 65535;
-  c.next = 0;
-  print_list(&a);
-  return 0;
+    /*
+     * Given a pointer to the first node of a linked list,
+     * insert a new node containing i into the beginning of the list
+     *
+     * Returns the node inserted (which is also the start of the list).
+     */
+    struct node *new_node = malloc(sizeof(struct node));
+    new_node->i = i;
+    new_node->next = target;
+    return new_node;
+};
+
+struct node * free_list(struct node *head)
+{
+    /*
+     * Frees all memory used by a linked list that starts with the given node 
+     * 
+     * Returns a pointer to the beginning of a list
+     */
+    struct node *cur_node = head;
+    struct node *temp;
+    while (cur_node)
+    {
+        temp = cur_node;
+        cur_node = cur_node->next;
+        printf("Freeing %p, holding %d\n", temp, temp->i);
+        free(temp);
+    }
+    return head;
 }
